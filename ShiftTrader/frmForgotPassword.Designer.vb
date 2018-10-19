83780ee400bf03c0768071870b82652991e54a42
Abigail Bashore <abibasho@cs.du.edu>
2018-10-19 00:51:01 -0600

    Added forgot password form

---
+++ ShiftTrader/frmForgotPassword.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmForgotPassword
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblUserName = New System.Windows.Forms.Label()
+        Me.lblNewPassword = New System.Windows.Forms.Label()
+        Me.lblConfirmPassword = New System.Windows.Forms.Label()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtNewPassword = New System.Windows.Forms.TextBox()
+        Me.txtConfirmPassword = New System.Windows.Forms.TextBox()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblUserName
+        '
+        Me.lblUserName.AutoSize = True
+        Me.lblUserName.Location = New System.Drawing.Point(30, 53)
+        Me.lblUserName.Name = "lblUserName"
+        Me.lblUserName.Size = New System.Drawing.Size(83, 17)
+        Me.lblUserName.TabIndex = 0
+        Me.lblUserName.Text = "User Name:"
+        '
+        'lblNewPassword
+        '
+        Me.lblNewPassword.AutoSize = True
+        Me.lblNewPassword.Location = New System.Drawing.Point(30, 85)
+        Me.lblNewPassword.Name = "lblNewPassword"
+        Me.lblNewPassword.Size = New System.Drawing.Size(104, 17)
+        Me.lblNewPassword.TabIndex = 1
+        Me.lblNewPassword.Text = "New Password:"
+        '
+        'lblConfirmPassword
+        '
+        Me.lblConfirmPassword.AutoSize = True
+        Me.lblConfirmPassword.Location = New System.Drawing.Point(30, 115)
+        Me.lblConfirmPassword.Name = "lblConfirmPassword"
+        Me.lblConfirmPassword.Size = New System.Drawing.Size(125, 17)
+        Me.lblConfirmPassword.TabIndex = 2
+        Me.lblConfirmPassword.Text = "Confirm Password:"
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(161, 50)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(182, 22)
+        Me.txtUserName.TabIndex = 3
+        '
+        'txtNewPassword
+        '
+        Me.txtNewPassword.Location = New System.Drawing.Point(161, 82)
+        Me.txtNewPassword.Name = "txtNewPassword"
+        Me.txtNewPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtNewPassword.Size = New System.Drawing.Size(182, 22)
+        Me.txtNewPassword.TabIndex = 4
+        '
+        'txtConfirmPassword
+        '
+        Me.txtConfirmPassword.Location = New System.Drawing.Point(161, 112)
+        Me.txtConfirmPassword.Name = "txtConfirmPassword"
+        Me.txtConfirmPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtConfirmPassword.Size = New System.Drawing.Size(182, 22)
+        Me.txtConfirmPassword.TabIndex = 5
+        '
+        'btnOK
+        '
+        Me.btnOK.Location = New System.Drawing.Point(199, 187)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(91, 28)
+        Me.btnOK.TabIndex = 6
+        Me.btnOK.Text = "OK"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(296, 187)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(91, 28)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmForgotPassword
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(393, 227)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnOK)
+        Me.Controls.Add(Me.txtConfirmPassword)
+        Me.Controls.Add(Me.txtNewPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.lblConfirmPassword)
+        Me.Controls.Add(Me.lblNewPassword)
+        Me.Controls.Add(Me.lblUserName)
+        Me.Name = "frmForgotPassword"
+        Me.Text = "frmForgotPassword"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblUserName As Label
+    Friend WithEvents lblNewPassword As Label
+    Friend WithEvents lblConfirmPassword As Label
+    Friend WithEvents txtUserName As TextBox
+    Friend WithEvents txtNewPassword As TextBox
+    Friend WithEvents txtConfirmPassword As TextBox
+    Friend WithEvents btnOK As Button
+    Friend WithEvents btnCancel As Button
+End Class
